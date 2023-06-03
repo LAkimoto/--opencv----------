f27c0c3bb4ca18c37cf939f46c56b7226598350f
LAkimoto <2428316292@qq.com>
2023-06-03 16:35:52 +0800

    改变游戏形式，增加细节

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-代码手势识别部分来自大佬[项目地址](https://github.com/lzane/Fingers-Detection-using-OpenCV-and-Python)
+代码手势识别部分来自大佬
+> [项目地址](https://github.com/lzane/Fingers-Detection-using-OpenCV-and-Python)
 
-本人水平有限，代码有一堆bug
+- 本人水平有限，代码有一堆bug
+
+- 通过猜拳衍生出赌桌游戏，胜利筹码指数级增长，失败立刻清除所有筹码，平局赠送10筹码
+
+- 添加记录，记录下最高筹码
+
 
 打包使用pyinstaller
 ```python
-pyinstaller --onefile --noconsole gui.py
+pyinstaller --onefile --noconsole gui.py #onefile打包需要在文件目录下自己创建一个rank文件
 ```
--- gui.py
+++ gui.py
@@ -20,6 +20,7 @@ cnt = -1
 
 
 class Game:
+    rank=Label()
     def __init__(self):
         self.game = Frame(root, bg='#F5F5F5')
         self.game.pack(fill=BOTH, expand=True)
@@ -36,6 +37,12 @@ class Game:
                           bg='#909399', fg='white', activebackground='#A6A6A6',
                           relief='flat', overrelief='raised', command=self.GameQuit)
         quit_btn.pack(pady=10)
+
+        self.rank = Label(self.game, text='筹码记录', font=('微软雅黑', 40), bg='#F5F5F5', fg='#263238')
+        self.rank.pack(pady=50)
+        file=open('rank','r')
+        self.rank["text"]="记录："+file.read()
+        file.close()
 
         root.mainloop()
 
@@ -54,7 +61,7 @@ class Game:
         title_bar.columnconfigure(0, weight=1)
 
         content = Label(game_help, bg='#37474F', fg='white', font=('微软雅黑', 12),
-                        text='欢迎来到猜拳游戏！\n\n在开始游戏前，请先校准背景，按下“确定”按钮。'
+                        text='欢迎来到赌徒游戏！\n\n在开始游戏前，请先校准背景，按下“确定”按钮。'
                              '\n\n游戏会在三秒钟后自动开始。\n\nTIPS:不出拳会默认帮你出石头哦')
         content.pack(expand=True, pady=10)
 
@@ -64,7 +71,7 @@ class Game:
         confirm_btn.pack(pady=15)
 
         def start():
-            # new.game(cap)
+            root.withdraw()#按下确定隐藏主页面
             game_help.destroy()
             video = Toplevel(self.game)
             video.geometry('800x800')
@@ -73,7 +80,7 @@ class Game:
                          text='确保方框中没有多余的可移动事物后点击开始进行游戏\n\nTIPS:包括你的头哦，还有尽量在干净的背景下进行游戏')
             tips.pack(pady=10)
             rules = Label(video, bg='#37474F', fg='white', font=('微软雅黑', 16),
-                         text='平局加1分，赢下一场加2分，输掉一场扣2分')
+                         text='平局加10筹码！赢下一场筹码以指数翻倍！输掉一场失去所有的筹码！')
             rules.pack(pady=10)
             btn_group = Frame(video)
             btn_group.pack()
@@ -83,7 +90,7 @@ class Game:
             btn_ok.grid(row=0, column=0)
             btn_exit = Button(btn_group, text='返回', width=20, height=2, font=('微软雅黑', 16),
                               bg='#E6A23C', fg='white', activebackground='#FFB900',
-                              relief='flat', overrelief='raised', command=lambda: video.destroy())
+                              relief='flat', overrelief='raised', command=lambda: callback(video))
             btn_exit.grid(row=0, column=1)
 
         def cf(window):  #
@@ -92,34 +99,30 @@ class Game:
             isBgCaptured = 1
             window.withdraw();
             MainGame(window)
+        def callback(video):
+            video.destroy()
+            file = open('rank', 'r')
+            self.rank["text"] = "记录：" + file.read()
+            file.close()
+            root.update()
+            root.deiconify()
 
         def MainGame(video):
             game = Toplevel(self.game)
             game.geometry('600x600')
+
             game.config(background="#F5F5F5")
             countdown = Label(game, font=("微软雅黑", 40))
-            countdown.pack(pady=50)
+            countdown.pack(pady=10)
             score=Label(game,text="准备！",font=("微软雅黑", 30))
-            score.pack(pady=20)
+            score.pack(pady=10)
             global scorenum
             scorenum = 2
             global randnum
+            global gesture
+            gesture = ""
             randnum = random.randint(0, 2)
 
-            def count(c):
-                global randnum
-                countdown["text"] = "{:.1f}".format(c)
-                game.config(background="#F5F5F5")
-                if c > 0.1:
-                    game.after(100, count, c - 0.1)
-                else:
-                    c = round(random.uniform(0.5, 2.0), 1)
-                    randnum = random.randint(0, 2)
-                    refresh()
-                    game.config(bg="#ed5736")
-                    game.after(1000, count, c)
-
-            gesture = ""
             info = Label(game, font=("微软雅黑", 20))
             info.pack(pady=30)
             info2 = Label(game, font=("微软雅黑", 20))
@@ -129,7 +132,22 @@ class Game:
                                 relief='flat', overrelief='raised', command=lambda :close())
             return_btn.pack(pady=10)
 
+            def count(c):
+                global randnum
+                countdown["text"] = "{:.1f}".format(c)
+                game.config(background="#F5F5F5")
+                if c > 0.1:
+                    game.after(100, count, c - 0.1)
+                else:
+                    # c = round(random.uniform(0.5, 2.0), 1)
+                    randnum = random.randint(0, 2)
+                    refresh()
+                    judge(gesture)
+                    # game.config(bg="#ed5736")
+                    # game.after(1000, count, c)
+
             def refresh():
+                global gesture
                 if (cnt == 0):
                     gesture = "石头"
                 elif (cnt == 1):
@@ -139,31 +157,11 @@ class Game:
                 else:
                     gesture = "啥？"
                 info["text"] = "您出的是" + gesture
-                judge(gesture)
+                # judge(gesture)
                 # game.after(50, refresh, )
 
             def judge(ges):
                 global scorenum
-                if (scorenum <= 0 ):
-                    def close_win():
-                        close()
-                        fail.destroy()
-                    fail = Toplevel(self.game)
-                    fail.geometry('300x300')
-                    message = Label(fail, text="很遗憾您输掉了比赛\n正在返回准备界面",font=('微软雅黑', 20))
-                    message.pack(pady=10)
-                    game.destroy()
-                    fail.after(3000,close_win)
-                elif(scorenum>=10):
-                    def close_win():
-                        close()
-                        fail.destroy()
-                    fail = Toplevel(self.game)
-                    fail.geometry('300x300')
-                    message = Label(fail, text="恭喜通关\n正在返回准备界面",font=('微软雅黑', 20))
-                    message.pack(pady=10)
-                    game.destroy()
-                    fail.after(3000,close_win)
 
                 result = cnt - randnum
                 # g=""
@@ -174,25 +172,58 @@ class Game:
                 else:
                     g="布"
                 if result == 0:
-                    info2["text"] = "电脑和您出的都是：" + ges + " 平局"
-                    scorenum+=1
-                elif result in (1, -2):
-                    info2["text"] = "电脑出：" + g + " 您输了"
-                    scorenum-=2
-                else:
-                    info2["text"] = "电脑出：" + g + " 您赢了"
+                    info2["text"] = "电脑和您出的都是：" + ges + " 平局，筹码加10"
                     scorenum+=2
-                score["text"] = "得分：" + str(scorenum)
+                    score["text"] = "您的筹码为：" + str(scorenum)
+                elif result in (1, -2):
+                    info2["text"] = "电脑出：" + g + " 您输了，失去所有筹码！"
+                    scorenum=0
+                    score["text"] = "您的筹码为：" + str(scorenum)
+                else:
+                    info2["text"] = "电脑出：" + g + " 您赢了，筹码以指数翻倍！"
+                    scorenum=scorenum**2
+                    score["text"] = "您的筹码为：" + str(scorenum)
 
+                if (scorenum <= 0 ):
+                    def close_win():
+                        close()
+                        fail.destroy()
+                    fail = Toplevel(self.game)
+                    fail.geometry('300x300')
+                    message = Label(fail, text="很遗憾您输掉了所有赌注\n正在返回准备界面",font=('微软雅黑', 20))
+                    scorenum=10
+                    message.pack(pady=10)
+                    game.destroy()
+                    fail.after(3000,close_win)
+                elif(scorenum>0):
+                    def close_win():
+                        with open('rank','w') as file:
+                            file.write(str(scorenum))
+                        file.close()
+                        close()
+                        fail.destroy()
+                    def suoha():
+                        count(1.5)
+                        fail.destroy()
+                    fail = Toplevel(self.game)
+                    fail.geometry('300x300')
+                    message = Label(fail, text="继续吗?\n您的筹码还有："+str(scorenum),font=('微软雅黑', 20))
+                    message.pack(pady=10)
+                    btn_continue=Button(fail,text="我就要梭哈！",command=lambda :suoha())
+                    btn_continue.pack(pady=5)
+                    btn_settlement=Button(fail,text="可以了，见好就收!",command=close_win)
+                    btn_settlement.pack(pady=5)
+                    # game.destroy()
 
             def close():
                 global isBgCaptured
-                video.deiconify()
+                video.deiconify() #回到准备界面
                 game.destroy()
                 isBgCaptured=0
                 new.reset()
 
             count(2.5)
+            game.mainloop()
 
     def GameQuit(self):
         root.quit()
